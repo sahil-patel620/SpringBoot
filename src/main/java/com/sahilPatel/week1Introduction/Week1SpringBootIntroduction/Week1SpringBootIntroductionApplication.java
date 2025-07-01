dcb4bb332b18c0e15430bf77625fbe99f0e00367
sahil-patel620 <Sp453771@gmail.com>
2025-07-01 23:42:06 +0530

    Week1 introduction to Springboot

---
+++ src/main/java/com/sahilPatel/week1Introduction/Week1SpringBootIntroduction/Week1SpringBootIntroductionApplication.java
@@ -0,0 +1,13 @@
+package com.sahilPatel.week1Introduction.Week1SpringBootIntroduction;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Week1SpringBootIntroductionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Week1SpringBootIntroductionApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sahilPatel/week1Introduction/Week1SpringBootIntroduction/Week1SpringBootIntroductionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sahilPatel.week1Introduction.Week1SpringBootIntroduction;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Week1SpringBootIntroductionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
