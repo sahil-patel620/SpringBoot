4621b0d3c7d2b79466942d5f975db691619a4391
sahil-patel620 <Sp453771@gmail.com>
2025-07-13 00:12:12 +0530

    week 1 homework

---
+++ HomeWork/AliceAndBakery/src/main/java/com/Week1HomeWork/AliceAndBakery/AliceAndBakeryApplication.java
@@ -0,0 +1,13 @@
+package com.Week1HomeWork.AliceAndBakery;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AliceAndBakeryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AliceAndBakeryApplication.class, args);
+	}
+
+}
---
+++ HomeWork/AliceAndBakery/src/test/java/com/Week1HomeWork/AliceAndBakery/AliceAndBakeryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Week1HomeWork.AliceAndBakery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AliceAndBakeryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
