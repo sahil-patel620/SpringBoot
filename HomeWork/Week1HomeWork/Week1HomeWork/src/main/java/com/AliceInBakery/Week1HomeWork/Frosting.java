3cc9a3f5bc70ceec521d14bb0e77483717b24cbb
sahil-patel620 <Sp453771@gmail.com>
2025-07-13 14:38:21 +0530

    Week1 HomeWork Interfaces

---
+++ HomeWork/Week1HomeWork/Week1HomeWork/src/main/java/com/AliceInBakery/Week1HomeWork/Frosting.java
@@ -0,0 +1,5 @@
+package com.AliceInBakery.Week1HomeWork;
+
+public interface Frosting {
+    public void getFrosting();
+}
---
+++ HomeWork/Week1HomeWork/Week1HomeWork/src/main/java/com/AliceInBakery/Week1HomeWork/Syrup.java
@@ -0,0 +1,5 @@
+package com.AliceInBakery.Week1HomeWork;
+
+public interface Syrup {
+    public void getSyrup();
+}
