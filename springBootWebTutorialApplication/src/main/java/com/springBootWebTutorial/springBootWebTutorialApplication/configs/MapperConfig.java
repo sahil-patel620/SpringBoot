e965fb20389f1fe6582a3edec9725bc2fc452581
sahil-patel620 <Sp453771@gmail.com>
2025-07-27 16:18:54 +0530

    Service layer, writing our business logic

--- springBootWebTutorialApplication/src/main/java/com/springBootWebTutorial/springBootWebTutorialApplication/configs/MapperConfig.java
+++ springBootWebTutorialApplication/src/main/java/com/springBootWebTutorial/springBootWebTutorialApplication/configs/MapperConfig.java
@@ -6,8 +6,9 @@ import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class MapperConfig {
+
     @Bean
     public ModelMapper getModelMapper(){
-        return new ModelMapper();
+        return new ModelMapper( );
     }
 }
