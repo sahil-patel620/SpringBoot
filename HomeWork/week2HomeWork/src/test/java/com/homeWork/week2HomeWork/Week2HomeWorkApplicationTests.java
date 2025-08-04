a6c3b3d74674fe76a6d09efae5a4ea0173ddb320
sahil-patel620 <Sp453771@gmail.com>
2025-08-04 15:11:54 +0530

    Week 2 HomeWork

---
+++ HomeWork/week2HomeWork/src/test/java/com/homeWork/week2HomeWork/Week2HomeWorkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.homeWork.week2HomeWork;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Week2HomeWorkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
