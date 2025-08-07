5f4d428ffd49dee70c12144503152663a3cf9b16
sahil-patel620 <Sp453771@gmail.com>
2025-08-07 15:21:12 +0530

    JPA Tutorial

---
+++ jpaTuts/src/main/resources/data.sql
@@ -0,0 +1,6 @@
+insert into test.product_table (price,quantity,sku,title_x)
+values
+(12.4, 4,"parle734","Parle Biscuit"),
+(14.4, 1, "pepsi123", "Pepsi" ),
+(34.4, 1, "cola124", "Coca Cola" ),
+(16.4, 3, "pepsi156", "Mazza");
