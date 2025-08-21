2ea8cea1f52614cf87bac18270c0b004d049ec4f
sahil-patel620 <Sp453771@gmail.com>
2025-08-22 01:43:12 +0530

    SpringBoot Basic Auditing and hibernate Envers

--- prod-ready-features/src/main/java/com/sahil/prod_ready_features/prod_ready_features/entities/PostEntity.java
+++ prod-ready-features/src/main/java/com/sahil/prod_ready_features/prod_ready_features/entities/PostEntity.java
@@ -2,6 +2,8 @@ package com.sahil.prod_ready_features.prod_ready_features.entities;
 
 import jakarta.persistence.*;
 import lombok.*;
+import org.hibernate.envers.Audited;
+import org.hibernate.envers.NotAudited;
 import org.springframework.data.annotation.CreatedBy;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedBy;
@@ -16,11 +18,28 @@ import java.time.LocalDateTime;
 @AllArgsConstructor
 @Getter
 @Setter
+@Audited
 public class PostEntity extends  AuditableEntity{
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String title;
+//    @NotAudited
     private String description;
+
+//    @PrePersist
+//    void beforeSave(){
+//
+//    }
+//
+//    @PreUpdate
+//    void beforeUpdate(){
+//
+//    }
+//
+//    @PreRemove
+//    void beforeRemove(){
+//
+//    }
 }
