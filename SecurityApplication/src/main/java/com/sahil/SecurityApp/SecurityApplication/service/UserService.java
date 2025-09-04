ff7a421d6d6f7f8a29ba4b925784fb244d2a9076
sahil-patel620 <Sp453771@gmail.com>
2025-09-05 00:07:03 +0530

    UserDetails, UserDetailsService and its repository is added

---
+++ SecurityApplication/src/main/java/com/sahil/SecurityApp/SecurityApplication/service/UserService.java
@@ -0,0 +1,23 @@
+package com.sahil.SecurityApp.SecurityApplication.service;
+
+import com.sahil.SecurityApp.SecurityApplication.exception.ResourceNotFoundException;
+import com.sahil.SecurityApp.SecurityApplication.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class UserService implements UserDetailsService {
+
+    private final UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        return userRepository.findByEmail()
+                .orElseThrow(()-> new ResourceNotFoundException("User with email "+ username +" not found."));
+    }
+}
+
