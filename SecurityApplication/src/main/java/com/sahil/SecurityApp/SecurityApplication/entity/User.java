6b7aad7ba2864fbaf0eeb33395c89e12fe17a56c
sahil-patel620 <Sp453771@gmail.com>
2025-09-07 17:40:13 +0530

    JWT getToken and generate token added

--- SecurityApplication/src/main/java/com/sahil/SecurityApp/SecurityApplication/entity/User.java
+++ SecurityApplication/src/main/java/com/sahil/SecurityApp/SecurityApplication/entity/User.java
@@ -1,13 +1,21 @@
 package com.sahil.SecurityApp.SecurityApplication.entity;
 
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 
 import java.util.Collection;
 import java.util.List;
 
+@Getter
+@Setter
 @Entity
+@NoArgsConstructor
+@AllArgsConstructor
 public class User implements UserDetails {
 
     @Id
