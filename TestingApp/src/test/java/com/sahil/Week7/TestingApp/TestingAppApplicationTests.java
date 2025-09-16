42dede8c1857895140c469421455bd3b6d684e28
sahil-patel620 <Sp453771@gmail.com>
2025-09-17 01:11:40 +0530

    Understanding JUnit Testing Framework and AssertJ Library

--- TestingApp/src/test/java/com/sahil/Week7/TestingApp/TestingAppApplicationTests.java
+++ TestingApp/src/test/java/com/sahil/Week7/TestingApp/TestingAppApplicationTests.java
@@ -1,13 +1,81 @@
 package com.sahil.Week7.TestingApp;
 
-import org.junit.jupiter.api.Test;
+import lombok.extern.slf4j.Slf4j;
+import org.assertj.core.api.Assertions;
+import org.assertj.core.data.Offset;
+import org.junit.jupiter.api.*;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+@Slf4j
 class TestingAppApplicationTests {
 
+    @BeforeEach
+    void setUp(){
+        log.info("Starting the method, setting up config");
+    }
+
+    @AfterEach
+    void tearDown(){
+        log.info("Tearing down the method");
+    }
+
+    @BeforeAll
+    static void setUpOnce(){
+        log.info("Setup Once ....");
+    }
+
+    @AfterAll
+    static void tearDownOnce(){
+        log.info("Tearing down all ....");
+    }
+
 	@Test
-	void contextLoads() {
+//    @Disabled
+	void testNumberOne() {
+        int a = 5;
+        int b = 3;
+        int result = addTwoNumber(a,b);
+
+//        Assertions.assertEquals(8,result);
+//        assertThat(result)
+//                .isEqualTo(8)
+//                .isCloseTo(9, Offset.offset(1));
+
+        assertThat("Apple")
+                .isEqualTo("Apple")
+                .startsWith("App")
+                .endsWith("l")
+                .hasSize(3);
+
 	}
 
+    int addTwoNumber(int a, int b){
+        return a+b;
+    }
+
+    @Test
+//    @DisplayName("displayTestNameTwo")
+    void testDivideTwoNumbers_whenDenominatorIsZero_ThenArithmeticException(){
+
+        int a = 5;
+        int b = 0;
+
+//        double result = divideTwoNumber(a,b);
+//        System.out.println(result);
+        assertThatThrownBy(()-> divideTwoNumber(a,b))
+                .isInstanceOf(ArithmeticException.class)
+                .hasMessage("Tried to divide by zero");
+    }
+
+    double divideTwoNumber(int a , int b){
+        try{
+            return a/b;
+        } catch (ArithmeticException e) {
+            log.error("Arithmethic Exception occurred: "+ e.getLocalizedMessage());
+            throw new ArithmeticException("Tried to divide by zero");
+        }
+    }
 }
