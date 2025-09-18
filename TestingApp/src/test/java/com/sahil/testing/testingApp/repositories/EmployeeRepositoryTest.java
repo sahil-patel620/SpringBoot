e4ab9d79e6788da4be4c539c7c517c612b3f34f3
sahil-patel620 <Sp453771@gmail.com>
2025-09-19 00:24:52 +0530

    Unit Testing the Persistence Layer and Setting up TestContainer

---
+++ TestingApp/src/test/java/com/sahil/testing/testingApp/repositories/EmployeeRepositoryTest.java
@@ -0,0 +1,61 @@
+    package com.sahil.testing.testingApp.repositories;
+
+    import com.sahil.testing.testingApp.TestContainerConfiguration;
+    import com.sahil.testing.testingApp.entities.Employee;
+    import org.junit.jupiter.api.BeforeEach;
+    import org.junit.jupiter.api.Test;
+    import org.springframework.beans.factory.annotation.Autowired;
+    import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+    import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+    import org.springframework.boot.test.context.SpringBootTest;
+    import org.springframework.context.annotation.Import;
+
+    import java.util.List;
+
+    import static org.assertj.core.api.Assertions.assertThat;
+
+    @Import(TestContainerConfiguration.class)
+    @DataJpaTest
+    //@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.ANY)
+    class EmployeeRepositoryTest {
+
+        @Autowired
+        private EmployeeRepository employeeRepository;
+
+        private Employee employee;
+
+        @BeforeEach
+        void setUp(){
+            employee = Employee.builder()
+                    .name("sahil")
+                    .email("sahil@gmail.com")
+                    .salary(100000L)
+                    .build();
+        }
+
+        @Test
+        void testFindByEmail_whenEmailISPresent_thenReturnEmployee() {
+
+            //Arrange, Given
+            employeeRepository.save(employee);
+
+            //Act, When
+            List<Employee> employeeList = employeeRepository.findByEmail(employee.getEmail());
+
+            //Assert, Then
+            assertThat(employeeList).isNotNull();
+            assertThat(employeeList).isNotEmpty();
+            assertThat(employeeList.get(0).getEmail()).isEqualTo(employee.getEmail());
+        }
+
+        @Test
+        void testFindByEmail_whenEmailISNotFound_thenReturnEmptyEmployeeList(){
+//            Given
+            String email = "notPresent.123@gmail.com";
+//            When
+            List<Employee> employeeList = employeeRepository.findByEmail(email);
+//            Then
+            assertThat(employeeList).isNotNull();
+            assertThat(employeeList).isEmpty();
+        }
+    }
